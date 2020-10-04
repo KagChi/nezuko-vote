57d821fd3a50b2173d3622a5cb68de1c6de0e2e6
カグチ <59391215+KagChi@users.noreply.github.com>
2020-10-04 13:06:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
 const DBL = require("dblapi.js");
-const dbl = new DBL(yourDBLTokenHere, { webhookPort: 5000, webhookAuth: 'password' });
+const dbl = new DBL("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjE2OTQ3MDI5MzA0OTM0NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk4MDc5MzM0fQ.S5a69--wCwnrhNsfG00EIbLPUu6R9a3N4JSB-Z5MB0g", { webhookPort: 5000, webhookAuth: 'password' });
 
 // Optional event
 
