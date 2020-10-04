6da2cbdc6daf7681b288e6905eff49108990dd21
カグチ <59391215+KagChi@users.noreply.github.com>
2020-10-04 12:55:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const DBL = require("dblapi.js");
-const dbl = new DBL('Your top.gg token', client);
+const dbl = new DBL(yourDBLTokenHere, { webhookPort: 5000, webhookAuth: 'password' });
 
-// Optional events
-dbl.on('posted', () => {
-  console.log('Server count posted!');
-})
+// Optional event
 
-dbl.on('error', e => {
- console.log(`Oops! ${e}`);
-})
-
-client.login("")
+dbl.webhook.on('ready', hook => {
+  console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+});
+dbl.webhook.on('vote', vote => {
+  console.log(`User with ID ${vote.user} just voted!`);
+});
