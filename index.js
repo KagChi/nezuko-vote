f81c4d8202e90f1943ab121ad383d101adf90f9a
カグチ <59391215+KagChi@users.noreply.github.com>
2020-10-04 15:16:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const DBL = require("dblapi.js");
 const dbl = new DBL("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjE2OTQ3MDI5MzA0OTM0NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk4MDc5MzM0fQ.S5a69--wCwnrhNsfG00EIbLPUu6R9a3N4JSB-Z5MB0g", { webhookPort: 5000, webhookAuth: 'password' });
 const Discord = require("discord.js");
 // Optional event
-
+const client = new Discord.Client();
 dbl.webhook.on('ready', hook => {
   console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
 });
