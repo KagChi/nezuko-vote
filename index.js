806fee750a64dfca28c5467b1dbb577b7a83db67
カグチ <59391215+KagChi@users.noreply.github.com>
2020-10-05 17:24:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const DBL = require("dblapi.js");
-const dbl = new DBL("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjE2OTQ3MDI5MzA0OTM0NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk4MDc5MzM0fQ.S5a69--wCwnrhNsfG00EIbLPUu6R9a3N4JSB-Z5MB0g", { webhookPort: 5000, webhookAuth: 'password' });
+const dbl = new DBL("token lu", { webhookPort: 3000, webhookAuth: 'password' });
 const Discord = require("discord.js");
 // Optional event
 const client = new Discord.Client();
@@ -11,7 +11,7 @@ const embed = new Discord.MessageEmbed()
 	.setTitle('NezukoChan#8098 Vote Log!')
   .addField(`Thanks ${client.users.cache.get(vote.user).username} For Vote NezukoChan!`)
 	.setColor('#FEFADC');
-const channel = client.channels.cache.get('222197033908436994');
+const channel = client.channels.cache.get('channel vote');
 	try {
 		const webhooks = await channel.fetchWebhooks();
 		const webhook = webhooks.first();
@@ -24,7 +24,6 @@ const channel = client.channels.cache.get('222197033908436994');
 	} catch (error) {
 		console.error('Error trying to send: ', error);
 	}
-  console.log(`User with ID ${vote.toString()} just voted!`);
 })
 
-client.login("NjE2MTY5NDcwMjkzMDQ5MzQ0.XWYqiQ.q8iu8yhWdhqPAze7VrqJpNsoVjk")
+client.login("token bot lu")
